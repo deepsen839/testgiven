10668359819c67d766f2d0150a52728d96b019f0
DeepSen <deepsen839@gmail.com>
2022-11-14 06:41:59 +0530

    second commit

--- list_compare.py
+++ list_compare.py
@@ -24,11 +24,11 @@ df5 = pd.DataFrame({
 counter = 0
 for partno,quantity in zip(BomData["partno"],BomData["quantity"]):
     select_indices = list(np.where(df3["partno"] == partno)[0])
-  
+
     if df3["partno"].values[select_indices].size > 0:
             # final_data_df.iloc[bomdata_index] = bomdf.iloc[bomdata_index]
         if df3["quantity"].values[select_indices][0] > quantity:
-          
+
             # print(bomdf["partno"][counter])
                 # if copy_of_disti_df["quantity"][select_indices][0] = quantity-Distidf["quantity"].values[select_indices][0]
             df4.at[counter,'partno'] = bomdf.at[counter,'partno']
@@ -52,7 +52,7 @@ for partno,quantity in zip(BomData["partno"],BomData["quantity"]):
             df5.at[counter,'partno'] = df3.at[select_indices[0],'partno']
             df5.at[counter,'quantity'] = df3.at[select_indices[0],'quantity']
             df3.at[select_indices[0],'quantity'] = df3.at[select_indices[0],'quantity']-quantity
-            
+
             # print(df4)
             # print(df5)
     else:
@@ -68,34 +68,6 @@ for index, row in df3.iterrows():
         df5.at[counter,"partno"] = row['partno']
         df5.at[counter,"quantity"] = row['quantity']
         counter = counter+1
-    # sorted_disti_datadf.iloc[select_indices]=
-    # print(np.where(Distidf["partno"] == partno),np.where(bomdf["partno"] == partno))
-    # print(Distidf["partno"].values[select_indices].flat[0])
-    # print(Distidf.iloc[select_indices].get("partno"))
-    # if select_index == np.where(Distidf["partno"] == partno):
-    #     select_index = np.where(bomdf["partno"] == partno)
-
-    
-    # sorted_disti_data['partno'] = []
-    # sorted_disti_data['quantity'] = []
-    dist_data_partno_value = ''
-    # print(partno)
-
-    # if partno in DistiData['partno']:
-        # finalpartno.append(partno)
-        # if partno in sorted_disti_data['partno']:
-        # if partno not in finalpartno:
-
-            # dist_data_partno_index = DistiData['partno'].index(partno)        
-            # dist_data_partno_value = DistiData["quantity"][dist_data_partno_index]
-        # DistiData['partno'][bomdata_index] = partno
-        # DistiData['quantity'][bomdata_index] = dist_data_partno_value
-
-        # if quantity < dist_data_partno_value:
-        #     sorted_disti_data['partno'].append(partno)
-        #     sorted_disti_data['quantity'].append(quantity)
-        #     remainder_disti_data['partno'].append(partno)
-        #     remainder_disti_data['quantity'].append(dist_data_partno_value-quantity)
     counter=counter+1
 
 
@@ -110,4 +82,3 @@ combineddf = pd.concat([df4, df5],axis=1, keys = ['BomData', 'Distidf'])
 print(combineddf)
 
 
-    
